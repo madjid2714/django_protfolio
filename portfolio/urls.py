de907b679610c5279aedc6f16e2d328ac1980787
madjid <mohamedmadjid.meddah@univ-mosta.dz>
2022-09-21 20:19:03 +0100

    [main]: add some  functions to our jobs views

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.madjid, name='madjid')
+    path('', jobs.views.madjid, name='madjid'),
+    path('jobs/<int:job_id>',jobs.views.detail, name='detail'),
 ] 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
